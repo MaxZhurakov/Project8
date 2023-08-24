f4d12b9e7d75cea8fe5ea985fb0c258086605441
Zhura_is65 <muxmax969@gmail.com>
2023-08-24 11:30:36 +0300

    commit

--- Project8/main.cpp
+++ Project8/main.cpp
@@ -1,80 +1,59 @@
-//HM_ex_1_26.03.2023 
+//HM_ex_1_8.04.2023
 #include <iostream>
 using namespace std;
-int Figura(int x, int y)
+int arraySum(int arr[], int n) 
 {
-    for (int i = 0; i < x; i++)
-    {
-        for (int j = 0; j < y; j++)
-        {
-            if (i == 0 || i == x - 1 || j == 0 || j == y - 1)
-            {
-                cout << char(219);
-            }
-            else
-            {
-                cout << char(255);
-            }
-        }
-        cout << endl;
-    }
-    return 0;
+    if (n == 0)
+        return 0;
+    else
+        return arr[n - 1] + arraySum(arr, n - 1);
 }
-int main()
-{
-    int X, Y;
-    cout << " Enter height " << endl;
-    cin >> X;
-    cout << " Enter width " << endl;
-    cin >> Y;
-    system("pause");
-    return 0;
-}
-//HM_ex_3_26.03.2023_
+//HM_ex_2_8.04.2023
 #include <locale>
 #include <stdio.h>
 int main()
 {
-    int num = 1;
-    int n;
-    setlocale(LC_CTYPE, "ukr");
-    printf("¬ведiть n=");
-    scanf_s("%i", &n);
-    for (int i = 2; i < n; ++i)
-        for (int a = 2; a < i; ++a)
-        {
-            if (i % a == 0)
-                break;
-            else if (i == a + 1)
-                num++;
-        }
-    printf("%i", num);
+    double power(double a, int n) 
+    
+    {
+
+        if (n == 0)
+
+            return 1;
+
+        else if (n < 0)
+
+            return 1 / power(a, -n);
+
+        else if (n % 2 == 0)
+
+            return power(a * a, n / 2);
+
+        else
+
+            return a * power(a, n - 1);
+}
+//HM_ex_3_8.04.2023 
+#include <iostream>
+using namespace std;
+int main
+{
+    int fibonacci(int n)
+{
+        if (n <= 1)
+            return n;
+        else
+            return fibonacci(n - 1) + fibonacci(n - 2);
+    }
     return 0;
 }
-//HM_ex_5_26.03.2023 
+//HM_ex_3_8.04.2023 
 #include <iostream>
-#include <iomanip>
-#include <vector>
-#include <fstream>
-#include <iterator>
-#include <algorithm>
 using namespace std;
-
-typedef istream_iterator<int> ii;
-
-int main(int argc, char* argv[])
+int factorial(int n) 
 {
-    fstream fin("input.txt", ios::in);
-
-    if (!fin)
-    {
-        cerr << "error: couldn't open file.\n";
-        exit(1);
-    }
-
-    vector<int> coll((ii(fin)), ii());
-    cout << *max_element(coll.begin(), coll.end());
-
-    cout << "\n\n";
-    return 0;
+if (n == 0)
+ return 1;
+ else
+ return n * factorial(n - 1);
 }
