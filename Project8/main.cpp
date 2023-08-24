03a5aa57964dd6e6e047c63c396ae4a01fe79b87
Zhura_is65 <muxmax969@gmail.com>
2023-08-24 11:40:05 +0300

    commit

--- Project8/main.cpp
+++ Project8/main.cpp
@@ -1,80 +1,35 @@
-//HM_ex_1_26.03.2023 
-#include <iostream>
+//HM_ex_1_10.04.2023
+//#include <iostream> 
+//#include <algorithm> 
+//using namespace std;
+//int main()
+//{
+//    const int length = 5;
+//    int array[length] = { 30, 50, 20, 10, 40 };
+//    std::sort(array, array + length);
+//    for (int i = 0; i < length; ++i)
+//        std::cout << array[i] << ' ';
+//    return 0;
+//}
+//HM_ex_2_10.04.2023
+#include <iostream> 
+#include <algorithm> 
 using namespace std;
-int Figura(int x, int y)
-{
-    for (int i = 0; i < x; i++)
-    {
-        for (int j = 0; j < y; j++)
-        {
-            if (i == 0 || i == x - 1 || j == 0 || j == y - 1)
-            {
-                cout << char(219);
-            }
-            else
-            {
-                cout << char(255);
-            }
-        }
-        cout << endl;
-    }
-    return 0;
-}
 int main()
 {
-    int X, Y;
-    cout << " Enter height " << endl;
-    cin >> X;
-    cout << " Enter width " << endl;
-    cin >> Y;
-    system("pause");
-    return 0;
-}
-//HM_ex_3_26.03.2023_
-#include <locale>
-#include <stdio.h>
-int main()
-{
-    int num = 1;
-    int n;
-    setlocale(LC_CTYPE, "ukr");
-    printf("¬ведiть n=");
-    scanf_s("%i", &n);
-    for (int i = 2; i < n; ++i)
-        for (int a = 2; a < i; ++a)
-        {
-            if (i % a == 0)
-                break;
-            else if (i == a + 1)
-                num++;
-        }
-    printf("%i", num);
-    return 0;
-}
-//HM_ex_5_26.03.2023 
-#include <iostream>
-#include <iomanip>
-#include <vector>
-#include <fstream>
-#include <iterator>
-#include <algorithm>
-using namespace std;
-
-typedef istream_iterator<int> ii;
-
-int main(int argc, char* argv[])
-{
-    fstream fin("input.txt", ios::in);
-
-    if (!fin)
+    const int length = 5;
+    int array[length] = { 30, 50, 20, 10, 40 };
+    for (int startIndex = 0; startIndex < length - 1; ++startIndex)
     {
-        cerr << "error: couldn't open file.\n";
-        exit(1);
+        int largestIndex = startIndex;
+        for (int currentIndex = startIndex + 1; currentIndex < length; ++currentIndex)
+        {
+            if (array[currentIndex] > array[largestIndex])
+                largestIndex = currentIndex;
+        }
+        std::swap(array[startIndex], array[largestIndex]);
     }
-
-    vector<int> coll((ii(fin)), ii());
-    cout << *max_element(coll.begin(), coll.end());
-
-    cout << "\n\n";
+    for (int index = 0; index < length; ++index)
+        std::cout << array[index] << ' ';
     return 0;
 }
